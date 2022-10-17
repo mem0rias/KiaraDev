048c4029d562b3b784b105200fd8e18e841ec92c
danbermudez <a01377242@itesm.mx>
2022-10-17 00:15:37 -0500

    Fix de propiedad

--- routes/propiedad.routes.js
+++ routes/propiedad.routes.js
@@ -3,12 +3,14 @@ const propiedadController = require('../controllers/propiedad.controller');
 
 const router = express.Router();
 
+//Rutas para agregar propiedades
+router.get('/registrar', propiedadController.get_new);
+router.post('/registrar', propiedadController.post_new);
+
 router.get('/', propiedadController.get_propiedades);
 router.get('/:id', propiedadController.get_one);
 
-//Rutas para agregar propiedades
-router.get('propiedad/new', propiedadController.get_new);
-router.post('propiedad/new', propiedadController.post_new);
+
 
 
 
