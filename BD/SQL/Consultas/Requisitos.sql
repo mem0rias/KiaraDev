b8e7e053378e5a1fb00d6bcbd9b5cb966533b439
mem0rias <79435043+mem0rias@users.noreply.github.com>
2022-10-16 20:40:26 -0500

    Actalizacion de BS

--- BD/SQL/Consultas/Requisitos.sql
+++ BD/SQL/Consultas/Requisitos.sql
@@ -1,1 +1,1 @@
-﻿SELECT r.tipo_doc, descripcion FROM requisitos r, tipo_doc td WHERE tipo_exp = 2 AND r.Tipo_Doc = td.Tipo_Doc
+﻿SELECT r.tipo_doc, descripcion FROM requisitos r, tipo_doc td WHERE tipo_exp = 3 AND r.Tipo_Doc = td.Tipo_Doc
