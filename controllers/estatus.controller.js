d34367fe7c7f3e4a061f962aec63abdda3c70161
AudreyNava <69495191+AudreyNava@users.noreply.github.com>
2022-10-20 00:44:22 -0500

    agregar estatus files

---
+++ controllers/estatus.controller.js
@@ -0,0 +1,3 @@
+const { request } = require('http');
+const path = require('path');
+const Recomendacion = require('../models/estatus.model');
---
+++ routes/estatus.routes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const estatusController = require('../controllers/estatus.controller');
+
+
+
+module.exports = router;
