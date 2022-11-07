8b21bcf2107c19e3483778049c86cb3427bc03cf
paulponcep <A01205866@itesm.mx>
2022-11-07 17:11:51 -0600

    algunos cambios jeje

--- models/resenas.model.js
+++ models/resenas.model.js
@@ -5,3 +5,19 @@ SELECT contenido
 FROM `comentarios` 
 WHERE idUsuario = 46;
 */
+
+module.exports = class Reseñas {
+    constructor(mi_comentario, mi_usuario, mi_contenido, mi_fecha, mi_aprobado) {
+        this.comentario = mi_comentario;
+        this.comentario = mi_usuario;
+        this.contenido = mi_contenido;
+        this.fecha = mi_fecha;
+        this.aprobado = mi_aprobado;
+
+    }
+    guardar() {
+        return db.execute(
+            'INSERT INTO comentarios (idComentario, idUsuario, Contenido, Fecha, Aprobado) VALUES (?, ?, ?, ?, ?)', [this.comentario, this.usuario, this.contenido, this.fecha, this.aprobado]);
+
+    }
+}
