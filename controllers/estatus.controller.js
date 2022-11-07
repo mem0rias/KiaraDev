818c6076078ddf65d87607d9dfccef94d4ac27ba
AudreyNava <audrey.nava@kalypso.com>
2022-11-07 17:13:24 -0600

    Empezando el show de hacelo dinamico

--- controllers/estatus.controller.js
+++ controllers/estatus.controller.js
@@ -16,4 +16,27 @@ exports.get_root = (request, response, next) => {
             console.log(error);
         });
 
+}
+
+exports.miEstatus = (request, response, next) => {
+    
+    let userid = request.session.IdUser;
+    expediente.GetRents(userid).then(([rows, fieldata]) =>{
+        expediente.GetBuy(userid).then(([rows2, fieldata2]) =>{
+            expediente.GetSelling(userid).then(([rows3, fieldata3]) =>{
+                let array = new Array();
+                let arraytipos = ['5','3','1'];
+                let funcs = new Array();
+                array.push(rows.length != 0);
+                array.push(rows2.length != 0);
+                array.push(rows3.length != 0);
+                console.log(array);
+                response.render('./Estatus/estatus', {arr: array});
+                
+                
+            })
+        })
+    })
+
+    
 };
--- models/estatus.model.js
+++ models/estatus.model.js
@@ -5,5 +5,17 @@ module.exports = class listEstatus{
     static fetchAll() {
         return db.execute('SELECT * FROM pasos_compra');
     }
+    
+    static GetRents(IdUsuario){
+        return db.execute('SELECT IdUsuario from cliente_pr_prop where IdUsuario = ?',[IdUsuario]);
+    }
+
+    static GetBuy(IdUsuario){
+        return db.execute('SELECT IdUsuario from cliente_pc_prop where IdUsuario = ?',[IdUsuario]);
+    }
+
+    static GetSelling(IdUsuario){
+        return db.execute('SELECT IdUsuario from asignacion where IdUsuario = ?',[IdUsuario]);
+    }
 
 }
