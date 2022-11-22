545d0b4246dbe9ed9ae58d1493fc607e4831b963
mem0rias <79435043+mem0rias@users.noreply.github.com>
2022-11-21 23:35:51 -0600

    modificacion conexcion a BD

--- util/database.js
+++ util/database.js
@@ -1,10 +1,18 @@
 const mysql = require('mysql2');
 
-const pool = mysql.createPool({
+/*const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'kiara',
     password: '',
+});
+*/
+
+const pool = mysql.createPool({
+    host: '165.232.152.1',
+    user: 'kiaradeveloper',
+    database: 'kiara',
+    password: 'kiara',
 });
 
 module.exports = pool.promise();
