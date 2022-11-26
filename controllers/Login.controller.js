81085c261909229519a8bbe1ded0f9eeb578d7aa
danbermudez <a01377242@itesm.mx>
2022-11-26 17:01:34 -0600

    Vistas

--- controllers/Login.controller.js
+++ controllers/Login.controller.js
@@ -42,7 +42,7 @@ exports.loginverf = (request, response, next) => {
                     request.session.IdRol = rows[0].IdRol;
                     request.session.Telefono = rows[0].Telefono;
                     request.session.Email = rows[0].email;
-                    request.session.NombreUser= rows[0].Nombre + rows[0].PA + rows[0].SA;
+                    request.session.NombreUser= rows[0].Nombre + ' ' + rows[0].PA;
                     
 
                     User.getPermisos(rows[0].IdUsuario)
@@ -51,7 +51,6 @@ exports.loginverf = (request, response, next) => {
                                     for (let permiso of permisos) {
                                         request.session.permisos.push(permiso.nombre);
                                         console.log(permiso);
-                        
                                     }
                                     
                                     return request.session.save(err => {
