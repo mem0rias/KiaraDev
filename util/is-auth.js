3ae6cb6b9240d651e55fba023f50698cedae592c
mem0rias <79435043+mem0rias@users.noreply.github.com>
2022-12-14 01:35:59 -0600

    Middlewares en dashboard

--- util/is-auth.js
+++ util/is-auth.js
@@ -3,6 +3,6 @@ module.exports = (request, response, next) => {
     if (!request.session.isLoggedIn) {
         request.session.info = 'Necesitas iniciar sesion primero';
         return response.redirect('/login');
-    }
-    next();
+    }else
+        next();
 }
