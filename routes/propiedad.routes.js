aed7274ed5dc3f098a6e2aaad96ad992da99a3b8
mem0rias <79435043+mem0rias@users.noreply.github.com>
2022-12-14 01:47:31 -0600

    Propiedades ya tiene middleware adecuado

--- routes/propiedad.routes.js
+++ routes/propiedad.routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const propiedadController = require('../controllers/propiedad.controller');
+const isAgente = require('../util/is-Agente');
 const isAuth= require('../util/is-auth.js');
 
 const router = express.Router();
@@ -7,18 +8,18 @@ const router = express.Router();
 //Rutas para agregar propiedades
 router.get('/imagenes/:valor_busqueda', propiedadController.get_Imagenes);
 
-router.get('/registrar', isAuth, propiedadController.get_new);
-router.post('/registrar', isAuth, propiedadController.post_new);
+router.get('/registrar', isAuth, isAgente, propiedadController.get_new);
+router.post('/registrar', isAuth, isAgente, propiedadController.post_new);
 
 router.get('/', propiedadController.get_propiedades);
 router.get('/:id', propiedadController.get_one);
 
-router.get('/editar/:id', isAuth, propiedadController.get_edit);
-router.post('/editar',  isAuth,propiedadController.post_edit);
+router.get('/editar/:id', isAuth, isAgente, propiedadController.get_edit);
+router.post('/editar',  isAuth, isAgente, propiedadController.post_edit);
 
 router.get('/buscar/:valor_busqueda', propiedadController.get_buscar);
 router.get('/buscarAsigandos/:valor_busqueda', propiedadController.get_buscarAsigandos);
 
-router.post('/borrar', isAuth, propiedadController.post_delete);
+router.post('/borrar', isAuth, isAgente, propiedadController.post_delete);
 
 module.exports = router;
