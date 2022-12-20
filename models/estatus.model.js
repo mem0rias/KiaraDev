20c129899057200ff03600c20dc46154bfe4e449
mem0rias <79435043+mem0rias@users.noreply.github.com>
2022-12-19 22:28:40 -0600

    Pendiente terminar borrar y cancelar proceso en dashboard

--- models/estatus.model.js
+++ models/estatus.model.js
@@ -61,6 +61,6 @@ module.exports = class listEstatus{
     }
 
     static FetchAsignados(IdPropiedad){
-        return db.execute('SELECT u.nombre, u.PA, u.SA, u.Telefono, u.email from usuario u, asignacion a where u.IdUsuario = a.IdUsuario and IdPropiedad = ?', [IdPropiedad]);
+        return db.execute('SELECT u.nombre, u.PA, u.SA, u.Telefono, u.IdUsuario, u.email from usuario u, asignacion a where u.IdUsuario = a.IdUsuario and IdPropiedad = ?', [IdPropiedad]);
     }
 }
