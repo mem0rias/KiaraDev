869084c55ea7ad00ea3efd78977a6f3c364ce82a
rutituti <ruthsolisvelasco@outlook.com>
2022-12-21 15:47:36 -0600

    Funcion de eliminar usuarios

--- routes/dashboard.routes.js
+++ routes/dashboard.routes.js
@@ -10,18 +10,24 @@ const router = express.Router();
 
 router.use(express.static(path.join(__dirname, '..','public')));
 
+router.post('/usuarios/borrar/', isAuth, isAdmin, dashboardController.delete_user);
+
 router.post('/usuarios/guardar/', isAuth, isAdmin, dashboardController.saveRol);
+
 router.get('/usuarios/buscar/:busc', isAuth, isAgent, dashboardController.get_search);
 
 router.get('/', isAuth, dashboardController.get_dashboard);
+
 router.get('/usuarios/roles', isAuth, isAdmin,dashboardController.get_roluserlist);
+
 router.get('/usuarios', isAuth, isAgent, dashboardController.get_userlist);
 
-
 router.get('/asignado', isAuth,dashboardController.get_propiedadesAsignadas);
+
 router.get('/MisPropiedades', isAuth, dashboardController.getPropiedades_Propias);
 
 router.get('/info', isAuth,dashboardController.get_Info);
+
 router.post('/info/actualizar', isAuth,dashboardController.post_Info);
 
 module.exports = router;
